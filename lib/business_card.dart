e2978d9fe5e0f3df29bf92f04e0578cd2154e70b
Дамир Даутов <damir.dautov@tages.ru>
2023-09-10 10:36:16 +0500

    Step 2

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -19,40 +19,61 @@ class BusinessCard extends StatefulWidget {
 }
 
 class _BusinessCardState extends State<BusinessCard> {
+  double _mousePosition = 0.5;
+
+  void _onPanUpdate(DragUpdateDetails e) {
+    setState(() {
+      _mousePosition = e.localPosition.dx / MediaQuery.sizeOf(context).width;
+    });
+  }
+
+  void _onMouseHover(PointerHoverEvent e) {
+    setState(() {
+      _mousePosition = e.localPosition.dx / MediaQuery.sizeOf(context).width;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Center(
-                child: Text(
-                  'Hello world!',
-                  style: TextStyle(color: Colors.white),
-                ),
+      body: GestureDetector(
+        onPanUpdate: _onPanUpdate,
+        child: MouseRegion(
+          onHover: _onMouseHover,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
+            child: Transform(
+              transform: Matrix4.identity()..setEntry(3, 0, (_mousePosition - 0.5) * 0.001),
+              alignment: Alignment.center,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Column(
+                      children: [
+                        CardAvatar(),
+                        CardHeader(),
+                        SizedBox(height: 16.0),
+                        CardInfoRow(icon: Icon(Icons.phone), text: '+78005553535'),
+                        SizedBox(height: 8.0),
+                        CardInfoRow(icon: Icon(Icons.email), text: 'email@email.com'),
+                        Spacer(),
+                        CardFooter(),
+                      ],
+                    ),
+                  ),
+                  // TODO: shader
+                ],
               ),
             ),
-            // TODO: shader
-          ],
+          ),
         ),
       ),
     );
   }
-
-  // void _onPanUpdate(DragUpdateDetails e) {
-  //   setState(() {
-  //     _mousePosition = e.localPosition.dx / MediaQuery.sizeOf(context).width;
-  //   });
-  // }
-  //
-  // void _onMouseHover(PointerHoverEvent e) {
-  //   setState(() {
-  //     _mousePosition = e.localPosition.dx / MediaQuery.sizeOf(context).width;
-  //   });
-  // }
-
-  // transform: Matrix4.identity()..setEntry(3, 0, (_mousePosition - 0.5) * 0.001),
 }
